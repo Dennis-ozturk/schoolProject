e28bb8386a0ad33e8530dc52ea489a7b974bd32c
MarcusAsp <mackan.a.b@hotmail.com>
2019-02-25 13:27:39 +0100

    product page added

---
+++ public/product-page.php
@@ -0,0 +1,21 @@
+<?php
+require_once('../includes/header.php');
+?>
+
+
+<section>
+    <section>
+        <h2><?php ?></h2>
+        <img src="<?php ?>" alt="">
+        <section>
+        <h4>Description</h4>
+        <p><?php ?></p>
+        <p>Price: <?php ?></p>
+        </section>
+    </section>
+</section>
+
+
+<?php
+require_once('../includes/footer.php');
+?>
