b96aea659d75d4e1e47a985681adcbeae28f85db
MarcusAsp <mackan.a.b@hotmail.com>
2019-02-27 14:37:46 +0100

    Mina ändringar

---
+++ public/product-category-page.php
@@ -0,0 +1,36 @@
+<?php
+require_once('../db/config.php');
+require_once('../includes/header.php');
+session_start();
+?>
+
+
+<Main class="category-container">
+
+<?php 
+
+$stmt = $conn->query("SELECT * FROM products");
+        while ($row = $stmt->fetch()){
+
+?>
+    <a class="product-card" href="product-page.php?product=<?php echo $row['productName'];?>">
+        <section name="<?php echo $row['productName']; ?>">
+            <img src="<?php ?>https://picsum.photos/300/300" alt="product-<?php echo $row['productCode'];?>">
+            <section>
+                <h2><?php echo $row['productName'];?></h2> <!-- Product name -->
+                <p><?php echo $row['productDescription'];?></p> <!-- Small description -->
+                <p>Price: $<?php echo $row['buyPrice'];?></p>
+                <p class="card-readmore">Read more</p>
+            </section>
+        </section>
+    </a>
+    <?php 
+    $_SESSION['productName'] = $_GET[''];
+    }
+    ?>
+</Main>
+
+
+<?php
+require_once('../includes/footer.php');
+?>
--- public/product-page.php
+++ public/product-page.php
@@ -1,21 +1,30 @@
 <?php
 require_once('../includes/header.php');
-
+session_start();
+print_r($_SESSION);
 
 ?>
 
 
-<section>
-    <section>
-        <h2><?php ?></h2>
-        <img src="<?php ?>" alt="product-<?php ?>">
+<Main class="product-container">
+    <a class="back-page" href="">Previous page</a>
+    <section class="product">
+        <img src="<?php ?>https://picsum.photos/600/500" alt="product-<?php ?>">
+        <div class="vertical-hr"></div>
         <section>
-        <h4>Description</h4>
-        <p><?php ?></p>
-        <p>Price: <?php ?></p>
+            <section>
+                <h1><?php ?>Header2</h1>
+                <h3>Description:</h3>
+                <p><?php ?>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a pharetra nisl. Donec facilisis orci consequat odio egestas fringilla. Sed posuere sit amet eros ac dignissim. Donec vel maximus dolor. Suspendisse lorem nisl, dapibus nec rutrum non, pulvinar eu lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed id finibus lorem. Suspendisse sit amet pellentesque quam. Pellentesque varius sodales odio, vitae semper erat molestie et. Suspendisse cursus nibh risus, eu tristique quam tempus nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            </section>
+            <p>Article Number: <?php ?></p>
+            <section>
+            <p>Price: $1894<?php ?></p>
+            <button>Add to cart</button>
+            </section>
         </section>
     </section>
-</section>
+</Main>
 
 
 <?php
